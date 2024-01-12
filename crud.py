4f719a430e5a4508cb3c5b92685a9eaee37731c7
noobed_max <siddiq@student.cialabs.tech>
2024-01-12 16:32:39 +0000

    Initial commit

---
+++ crud.py
@@ -0,0 +1,9 @@
+#sqlaclchemy 
+def create_image_metadata
+    # creates metadata by fetching key path etc
+def get_image_metadata
+ #returns metadat of the image from the db
+def update_image_metadata
+    #gets path and key to the image to be updated
+def delete_image_metadata
+    #it is what it is
