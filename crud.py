ee1fd4b90163a448f1b8e8400d38c0a23c879bb5
noobed_max <siddiq@student.cialabs.tech>
2024-01-18 19:05:00 +0000

    update

--- crud.py
+++ crud.py
@@ -1,13 +1,4 @@
-''' 
-def create_image_metadata
-    # creates metadata by fetching key path etc
-def get_image_metadata
- #returns metadat of the image from the db
-def update_image_metadata
-    #gets path and key to the image to be updated
-def delete_image_metadata
-    #it is what it is
-'''
+
 import sqlite3
 from fastapi import HTTPException
 
--- main.py
+++ main.py
@@ -1,34 +1,3 @@
-'''from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from fastapi.middleware.cors import CORSMiddleware
-import re
-#we will be using UploadFile class of fastapi
-
-
-app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # This allows all origins, you can specify your frontend URL instead
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-@app.post("/upload")
-    # Handle file upload logic, save file to server
-    # Save metadata to database
-
-@app.get("/retrieve/{key}")
-    # Retrieve file or metadata based on the key
-
-@app.put("/update/{key}")
-    # Handle file update logic, save new file to server
-    # Update metadata in the database
-
-@app.delete("/delete/{key}")
-    # Handle file deletion logic, remove file from server
-    # Delete metadata from the database
-'''
 
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import JSONResponse
