a0b3407be8f9ce48036490d1247414a0e899ca56
noobed-max <siddiq@student.cialabs.tech>
2024-01-24 00:23:34 +0530

    clientinterface

--- clientinterface.py
+++ clientinterface.py
@@ -1,4 +1,22 @@
 
+import os
+import subprocess
+from typing import List
 
-def save(key,value)
-    #hits the localhost at which a fastapi server is running which performs upload files and save the metadata to db , under @app.post("/upload/") , what the save key,value fucntion does is hits this withe the key and the file and performs the opertaion 
+def save(key, value: List[str]):
+    server_process = subprocess.Popen(["uvicorn", "main:app", "--host", "127.0.0.1", "--port", "8000"])
+    import time
+    time.sleep(5)
+    temp = "curl -X POST"
+    for string in value:
+        file_path = f'-F "files=@{string}"'
+
+        temp += f" {file_path}"
+    temp += f" http://127.0.0.1:8000/upload/?key={key}"
+    try:
+        subprocess.run(temp, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing command: {e}")
+key = "test3"
+files_to_upload = ["/home/deadsec/storage/bg.jpg", "/home/deadsec/storage/Storage_server.jpg"]
+save(key, files_to_upload)
