6ebba1851943e3e4b99cf84f5553eb3c05f41d2f
noobed_max <146673372+noobed-max@users.noreply.github.com>
2024-02-02 15:48:05 +0530

    Update clientinterface.py

--- clientinterface.py
+++ clientinterface.py
@@ -1,7 +1,4 @@
-import os
 import requests
-import base64
-API_URL = "http://127.0.0.1:8000"
 
 
 def save(API_URL, key, value):
@@ -12,22 +9,54 @@ def save(API_URL, key, value):
         if response.status_code == 200:
             print("Update successful:", response.json())
         else:
-            raise requests.HTTPError(response.text)
+            print("Error:", response.text)
+        return response 
 
+    except requests.HTTPError as e:
+        print("HTTPError:", e)
+         
+
+def get(API_URL, key):
+    try:
+        response= requests.get(f"{API_URL}/get/{key}")
+        if response.status_code == 200:
+            print("Update successful:", response.json())
+            return response.json()
+        else: 
+            print("Error:", response.json())
+            return response.json()
+       
     except requests.HTTPError as e:
         print("Error:", e)
 
-def retreieve(key):
+
+def update(API_URL,key,value,new_key=None):
     try:
-        response= requests.get(f"{API_URL}/retrieve/{key}")
+        data = {"new_key": new_key, "encoded_content": value}
+        response = requests.put(f"{API_URL}/update/?key={key}", data=data)
+
         if response.status_code == 200:
-            return response.json()
             print("Update successful:", response.json())
         else:
             raise requests.HTTPError(response.text)
 
     except requests.HTTPError as e:
         print("Error:", e)
+
+
+def delete(API_URL,key):
+    try:
+        response = requests.delete(f"{API_URL}/delete/?key={key}")
+
+        if response.status_code == 200:
+            print("Update successful:", response.json())
+        else:
+            raise requests.HTTPError(response.text)
+
+    except requests.HTTPError as e:
+        print("Error:", e)
+
+
 
 #key = "string_test"
 # Assuming value is a list of strings
